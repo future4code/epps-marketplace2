f44b83941d2f0fb8288976e2b57f081c5af4fc83
willfjr <willferreirajr@icloud.com>
2021-01-19 19:40:28 -0300

    Criei o componente subheader. Não consegui colocar os icones que queria.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { HomePage } from "./Pages/HomePage";
 import { SellersPage } from "./Pages/SellersPage";
+import Subheader from './Pages/components/subheader/Subheader'
 
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <>
         <HomePage />
+        <Subheader/>
         <SellersPage />
-      </div>
+      </>
     );
   }
 }
