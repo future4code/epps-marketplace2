ac48d9a1ce667ebae39f5590c5461b81ffbb3f7d
DouglasFlorido <crascaroth@gmail.com>
2021-01-20 14:42:45 -0300

    colocado box e arrumado filtro

--- src/Pages/SellersPage.js
+++ src/Pages/SellersPage.js
@@ -4,9 +4,7 @@ export class SellersPage extends Component {
   render() {
     return (
       <div>
-        <p>Sellers Page</p>
-        <p>Sellers Page</p>
-        <p>Sellers Page</p>
+        
       </div>
     )
   }
