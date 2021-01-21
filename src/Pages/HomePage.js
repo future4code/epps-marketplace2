6b2b9b42223417ba5b7d14e66838f127fd4cfafa
Felipe Moura <felipemimoura@gmail.com>
2021-01-21 14:18:14 -0300

    alteração dos locais da chamada de itens da api para homepage

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -5,11 +5,39 @@ import Subheader from './components/subheader/Subheader';
 import Filters from './components/filters/Filters';
 import RangeSlider from './components/filters/Range';
 import { Footer } from './components/footer/Footer'; 
-import ProductsCard from './components/ProductsCard/productsCard';
+import ProductsCard from './components/ProductsCard/ProductsCard';
+import axios from 'axios'
 
 
 export class HomePage extends React.Component {
+  state = {
+    produtos: []
+  }
+
+  informacoesCard = () => {
+
+    axios.get('https://us-central1-labenu-apis.cloudfunctions.net/fourUsedTwo/products')
+      .then((res) => {
+        
+        this.setState({ produtos: res.data.products })
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
+  componentDidMount = () => {
+    this.informacoesCard()
+  }
+
+  componentDidUpdate = () =>{
+    
+  }
+
+
+
   render() {
+    // console.log(this.state.produtos)
     return (
       <div className="App">
         <Header
@@ -21,7 +49,7 @@ export class HomePage extends React.Component {
           <Filters />
           <RangeSlider />
         </div>
-        <ProductsCard />
+        <ProductsCard  produtos={this.state.produtos}/>
         <Footer />
       </div>
         
