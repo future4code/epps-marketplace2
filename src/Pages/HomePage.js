9a50d1a5838c94827acdfde92577148c17659708
willfjr <willferreirajr@icloud.com>
2021-01-21 15:50:01 -0300

    modificações nos filtros

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -3,7 +3,6 @@ import React from 'react';
 import { Header } from './components/header/Header'
 import Subheader from './components/subheader/Subheader';
 import Filters from './components/filters/Filters';
-import RangeSlider from './components/filters/Range';
 import { Footer } from './components/footer/Footer'; 
 import ProductsCard from './components/ProductsCard/ProductsCard';
 import axios from 'axios'
@@ -33,22 +32,20 @@ export class HomePage extends React.Component {
   componentDidUpdate = () =>{
     
   }
-
-
+  // Pegar os produtos filtrados
+ 
 
   render() {
-    // console.log(this.state.produtos)
+    
+
     return (
       <div className="App">
         <Header
           goHome={this.props.goHome}
           handleHomePage={this.props.handleHomePage}
         />
+        <Filters/>
         <Subheader />
-        <div className="Container-filters">
-          <Filters />
-          <RangeSlider />
-        </div>
         <ProductsCard  produtos={this.state.produtos}/>
         <Footer />
       </div>
