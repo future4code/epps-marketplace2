9fb56b16e96b9ac6cb2ad73f8d4cd4e876fb5146
Felipe Moura <felipemimoura@gmail.com>
2021-01-22 16:08:09 -0300

    volta da lógica de ordenação

--- src/Pages/components/ProductsCard/ProductsCard.js
+++ src/Pages/components/ProductsCard/ProductsCard.js
@@ -6,6 +6,7 @@ const Principal = styled.div `
   flex-wrap: wrap;
   justify-content: space-around;
   align-items: center;
+  border: 1px solid red;
 `
 const Card = styled.div ` 
   border: 1px solid #4281a4;
