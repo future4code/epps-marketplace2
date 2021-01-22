268e05acc4631b5099416bc99fef4f1ddc7e8207
Felipe Moura <felipemimoura@gmail.com>
2021-01-22 11:20:46 -0300

    filtro de valores funcionandos

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,79 +1,106 @@
+import React from "react";
+import { Header } from "./components/header/Header";
+import Subheader from "./components/subheader/Subheader";
+import Filters from "./components/filters/Filters";
+import { Footer } from "./components/footer/Footer";
 
-import React from 'react';
-import { Header } from './components/header/Header'
-import Subheader from './components/subheader/Subheader';
-import Filters from './components/filters/Filters';
-import { Footer } from './components/footer/Footer'; 
-
-import ProductsCard from './components/ProductsCard/ProductsCard';
+import ProductsCard from "./components/ProductsCard/ProductsCard";
 // import CheckboxLabels from "./components/filters/FilterCheckBox";
-import axios from 'axios'
-
-
+import axios from "axios";
 
 export class HomePage extends React.Component {
   state = {
     produtos: [],
     produtosFiltrado: [],
-    valorMax: Infinity
-  }
+    valorMax: Infinity,
+    valorMin: 0,
+  };
 
   informacoesCard = () => {
-
-    axios.get('https://us-central1-labenu-apis.cloudfunctions.net/fourUsedTwo/products')
+    axios
+      .get(
+        "https://us-central1-labenu-apis.cloudfunctions.net/fourUsedTwo/products"
+      )
       .then((res) => {
-        
-        this.setState({ produtos: res.data.products })
+        this.setState({ produtos: res.data.products });
+        this.setState({ produtosFiltrado: res.data.products });
       })
       .catch((err) => {
-        console.log(err)
-      })
-  }
+        console.log(err);
+      });
+  };
 
   componentDidMount = () => {
-    this.informacoesCard()
-  }
-
-  componentDidUpdate = () =>{
+    this.informacoesCard();
     
-  }
+  };
+
+  componentDidUpdate = () => {
+    
+    
+  };
   // Pegar os produtos filtrados
- 
-  handleFilterMax = (e) =>{
-   
+
+  handleFilterMax = (e) => {
+    const valorMax = e.target.value
     this.setState({
-      valorMax: e.target.value
+      valorMax: valorMax
+    });
+  };
+  handleFilterMin = (e) =>{
+    // console.log(e.target.value)
+    const valorMinino = e.target.value
+    this.setState({
+      valorMin: valorMinino
     })
   }
 
-  pegaCategoria = (e) =>{
-    const categoria = e 
-    const filterCategoria = this.state.produtos.filter((produto) =>{
-        return(produto.category === categoria)
-      
+  filtraProdutos = () =>{
+    const listaProdutos = this.state.produtosFiltrado.filter((produto) =>{
+      return this.state.valorMin > 0 ? produto.price > this.state.valorMin : produto
       
     })
-    this.setState({
-      produtosFiltrado: filterCategoria
+    
+    .filter((produto) => {
+      return this.state.valorMax < Infinity ? produto.price < this.state.valorMax : produto
     })
+    console.log(listaProdutos)
+    return(
+      this.setState({
+        produtosFiltrado: listaProdutos,
+        valorMax: Infinity,
+        valorMin: 0
+      })
+    )
   }
 
+
+  pegaCategoria = (e) => {
+    const categoria = e;
+    const filterCategoria = this.state.produtos.filter((produto) => {
+    if (categoria === "home") {
+        return this.informacoesCard();
+      } else {
+        return produto.category === categoria;
+      }
+    });
+    this.setState({
+      produtosFiltrado: filterCategoria,
+    });
+  };
 
   render() {
-    
-
     return (
       <div className="App">
         <Header
           goHome={this.props.goHome}
           handleHomePage={this.props.handleHomePage}
         />
-        <Filters handleFilterMax={this.handleFilterMax} />
+        <Filters filtraProdutos={this.filtraProdutos} handleFilterMax={this.handleFilterMax} handleFilterMin={this.handleFilterMin} />
         <Subheader pegaCategoria={this.pegaCategoria} />
-        <ProductsCard produtos={this.state.produtosFiltrado}/>
+        <ProductsCard produtos={this.state.produtosFiltrado} />
         <Footer />
       </div>
-        
     );
   }
 }
